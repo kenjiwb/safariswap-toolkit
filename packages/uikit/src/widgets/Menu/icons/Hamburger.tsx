e0bea27d457c53119c3acd814981a9fb088a0ee7
Sigit Kuncoro <sigit.kunc@gmail.com>
2021-06-19 19:24:37 +0700

    fix(icon): Eslint error

--- packages/uikit/src/widgets/Menu/icons/Hamburger.tsx
+++ packages/uikit/src/widgets/Menu/icons/Hamburger.tsx
@@ -8,13 +8,13 @@ const Icon: React.FC<SvgProps> = (props) => {
       <defs>
         <style>{`.cls-1{fill:url(#linear-gradient);}.cls-2{fill:url(#linear-gradient-2);}.cls-3{fill:url(#linear-gradient-3);}`}</style>
         <linearGradient id="linear-gradient" x1="14.08" y1="60" x2="185.92" y2="60" gradientUnits="userSpaceOnUse">
-          <stop offset="0" stop-color="#3e473a" />
-          <stop offset="0.18" stop-color="#71723b" />
-          <stop offset="0.38" stop-color="#a39d3c" />
-          <stop offset="0.57" stop-color="#cbbf3c" />
-          <stop offset="0.74" stop-color="#e8d73d" />
-          <stop offset="0.89" stop-color="#f9e63d" />
-          <stop offset="1" stop-color="#ffeb3d" />
+          <stop offset="0" stopColor="#3e473a" />
+          <stop offset="0.18" stopColor="#71723b" />
+          <stop offset="0.38" stopColor="#a39d3c" />
+          <stop offset="0.57" stopColor="#cbbf3c" />
+          <stop offset="0.74" stopColor="#e8d73d" />
+          <stop offset="0.89" stopColor="#f9e63d" />
+          <stop offset="1" stopColor="#ffeb3d" />
         </linearGradient>
         <linearGradient id="linear-gradient-2" x1="14.08" y1="100" x2="185.92" y2="100" xlinkHref="#linear-gradient" />
         <linearGradient id="linear-gradient-3" x1="14.08" y1="140" x2="185.92" y2="140" xlinkHref="#linear-gradient" />
