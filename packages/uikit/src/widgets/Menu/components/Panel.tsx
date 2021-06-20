e6660c3867fc3b833552fae468da5f9edd51b8df
Sigit Kuncoro <sigit.kunc@gmail.com>
2021-06-20 11:41:01 +0700

    feat(panel): Added panel header

--- packages/uikit/src/widgets/Menu/components/Panel.tsx
+++ packages/uikit/src/widgets/Menu/components/Panel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PanelBody from "./PanelBody";
-import PanelFooter from "./PanelFooter";
+import PanelHeader from "./PanelHeader";
 import { SIDEBAR_WIDTH_REDUCED, SIDEBAR_WIDTH_FULL } from "../config";
 import { PanelProps, PushedProps } from "../types";
 
@@ -39,8 +39,8 @@ const Panel: React.FC<Props> = (props) => {
   const { isPushed, showMenu } = props;
   return (
     <StyledPanel isPushed={isPushed} showMenu={showMenu}>
+      <PanelHeader {...props} />
       <PanelBody {...props} />
-      <PanelFooter {...props} />
     </StyledPanel>
   );
 };
