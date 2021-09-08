dc7a030c48a2909419e327d0757b7dc1b6afe1b0
Beta Kurnia <yourgitemail@example.com>
2021-09-08 19:55:43 +0700

    feat: add balanceValue on menu

--- packages/uikit/src/widgets/Menu/components/PanelHeader.tsx
+++ packages/uikit/src/widgets/Menu/components/PanelHeader.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { CogIcon } from "../../../components/Svg";
 import IconButton from "../../../components/Button/IconButton";
-import { MENU_ENTRY_HEIGHT } from "../config";
 import { PanelProps, PushedProps } from "../types";
 import CakePrice from "./CakePrice";
 import LangSelector from "./LangSelector";
+import BalanceValue from './BalanceValue'
 
 interface Props extends PanelProps, PushedProps {}
 
 const Container = styled.div`
   flex: none;
-  padding: 8px 4px;
+  padding: 8px 4px ;
   background-color: transparent;
   border-bottom: solid 2px rgba(133, 133, 133, 0.1);
 `;
@@ -20,12 +20,12 @@ const SettingsEntry = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0px 8px 0px;
+  padding: 0 8px 8px;
 `;
 
-const SettingsBalanceEntry = styled.div`
-  display: flex;
-  padding: 0px 8px 8px;
+const BalanceText = styled.div`
+color: #ffffff;
+font-size: 12px;
 `;
 
 const PanelHeader: React.FC<Props> = ({ isPushed, pushNav, cakePriceUsd, currentLang, langs, setLang }) => {
@@ -39,22 +39,19 @@ const PanelHeader: React.FC<Props> = ({ isPushed, pushNav, cakePriceUsd, current
     );
   }
 
-
-
-const BalanceText = styled.div`
-color: #ffffff;
-padding-left: 32px;
-`;
-
   return (
     <Container>
       <SettingsEntry>
         <CakePrice cakePriceUsd={cakePriceUsd} />
         <LangSelector position="bottom" currentLang={currentLang} langs={langs} setLang={setLang} />
       </SettingsEntry>
-      <SettingsBalanceEntry>
-      <BalanceText>Balance</BalanceText>
-      </SettingsBalanceEntry>
+      <SettingsEntry>
+     
+      <BalanceText>Balance Credit</BalanceText>
+      </SettingsEntry>
+      <SettingsEntry style={{justifyContent: 'flex-start'}}>
+    <BalanceValue value={1000} />
+      </SettingsEntry>
     </Container>
   );
 };
