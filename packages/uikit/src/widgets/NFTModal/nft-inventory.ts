000b9278a6456f40d34baf5ca599947dba6af710
Beta Kurnia <yourgitemail@example.com>
2021-09-16 18:26:30 +0700

    feat: add nft modal

---
+++ packages/uikit/src/widgets/NFTModal/nft-inventory.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+// import Bruno from './nft-inventory/Bruno.jpeg'
+import Vulture from './nft-inventory/Vulture.jpeg'
+import Foxy from './nft-inventory/Foxy.jpeg'
+import Ele from './nft-inventory/Ele.jpeg'
+import Croc from './nft-inventory/Croc.jpeg'
+import Giraffy from './nft-inventory/Giraffy.jpeg'
+import Hippy from './nft-inventory/Hippy.jpeg'
+import Zebby from './nft-inventory/Zebby.jpeg'
+import Bruno from './nft-inventory/Bruno.jpeg'
+
+const NFTInventory = [
+  { name: 'Vulture', price: '100$', variant: 'Common', color: 'white', text: '#46373B', img: Vulture },
+  { name: 'Foxy', price: '500$', variant: 'Legend', color: '#d4af37', text: 'white', img: Foxy },
+  { name: 'Ele', price: '400$', variant: 'Epic', color: 'purple', text: 'white', img: Ele },
+  { name: 'Bruno', price: '200$', variant: 'Special', color: '#ff6700', text: 'white   ', img: Bruno },
+  { name: 'Croc', price: '100$', variant: 'Common', color: 'white', text: '#46373B', img: Croc },
+  { name: 'Giraffy', price: '300$', variant: 'Rare', color: 'blue', text: 'white', img: Giraffy },
+  { name: 'Hippy', price: '200$', variant: 'Special', color: '#ff6700', text: 'white', img: Hippy },
+  { name: 'Zebby', price: '100$', variant: 'Common', color: 'white', text: '#46373B', img: Zebby },
+]
+
+export default NFTInventory
