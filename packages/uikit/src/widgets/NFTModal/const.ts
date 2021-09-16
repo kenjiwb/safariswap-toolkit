bc660756572e3034369fdce52b07569f6e40de13
Beta Kurnia <yourgitemail@example.com>
2021-09-16 23:33:18 +0700

    feat: add import modal

---
+++ packages/uikit/src/widgets/NFTModal/const.ts
@@ -0,0 +1,15 @@
+export const columns = [
+    {
+      id: 1,
+      name: "id",
+    },
+    {
+      id: 2,
+      name: "pool",
+      label: "POOL",
+      render: ({ value }: { value: React.ReactNode }): React.ReactNode => value,
+    },
+  ];
+  
+  export const data = [];
+  
---
+++ packages/uikit/src/widgets/NFTModal/index.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+import Flex from "../../components/Box/Flex";
+import useNFTModal from "./useNFTModal";
+
+export default {
+  title: "Widgets/NFTModal",
+  argTypes: {},
+};
+
+export const Wallet: React.FC = () => {
+  const { onPresentNFTModal } = useNFTModal(
+    () => null,
+  );
+  return (
+    <Flex>
+      <Button onClick={onPresentNFTModal}>Open NFT modal</Button>
+    </Flex>
+  );
+};
