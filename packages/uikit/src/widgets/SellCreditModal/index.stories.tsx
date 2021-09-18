9a075268303f986829888d13b3ec9a5ad7cce4f6
Beta Kurnia <yourgitemail@example.com>
2021-09-18 13:27:30 +0700

    feat: add sell credit modal

---
+++ packages/uikit/src/widgets/SellCreditModal/index.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "../../components/Button/Button";
+import Flex from "../../components/Box/Flex";
+import useSellCreditModal from "./useSellCreditModal";
+
+export default {
+  title: "Widgets/SellCreditModal",
+  argTypes: {},
+};
+
+export const Wallet: React.FC = () => {
+  const { onPresentSellCreditModal } = useSellCreditModal(
+    () => null,
+  );
+  return (
+    <Flex>
+      <Button onClick={onPresentSellCreditModal}>Open Sell Credit modal</Button>
+    </Flex>
+  );
+};
