c5e162b0457fd56cb772ef849fc74c02abfe9588
Beta Kurnia <betakurnia1903@gmail.com>
2021-09-26 17:03:34 +0700

    feat: add question mark icons on menu

--- packages/uikit/src/widgets/Menu/icons/index.ts
+++ packages/uikit/src/widgets/Menu/icons/index.ts
@@ -26,3 +26,4 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as QuestionMarkIcon } from "./QuestionMark";
