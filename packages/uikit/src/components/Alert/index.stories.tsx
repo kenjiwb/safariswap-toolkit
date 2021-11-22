ede9f7e84864cee03ef8186956702b2d46831ad5
vishalhp01 <vishal.upwork.hp@gmail.com>
2021-11-22 12:16:38 +0530

    fix(changes): Toster ui changes

--- packages/uikit/src/components/Alert/index.stories.tsx
+++ packages/uikit/src/components/Alert/index.stories.tsx
@@ -21,22 +21,30 @@ export const Default: React.FC = () => {
     <div style={{ padding: "32px", width: "400px" }}>
       <Row>
         <Alert title="Info">
-          <Text as="p">This is a description</Text>
+          <Text style={{ color: "black" }} as="p">
+            This is a description
+          </Text>
         </Alert>
       </Row>
       <Row>
         <Alert title="Success" variant="success">
-          <Text as="p">This is a description</Text>
+          <Text style={{ color: "black" }} as="p">
+            This is a description
+          </Text>
         </Alert>
       </Row>
       <Row>
         <Alert title="Warning" variant="warning">
-          <Text as="p">This is a description</Text>
+          <Text style={{ color: "black" }} as="p">
+            This is a description
+          </Text>
         </Alert>
       </Row>
       <Row>
         <Alert title="Danger" variant="danger">
-          <Text as="p">This is a description</Text>
+          <Text style={{ color: "black" }} as="p">
+            This is a description
+          </Text>
         </Alert>
       </Row>
     </div>
